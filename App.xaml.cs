3ea15cfd5cdaa66f8df30c6c2d4c6cceaf94cf09
Künzler, Nico <Nico.Kuenzler@audius.de>
2024-03-27 08:23:22 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiNightlyCrash
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage();
+            MainPage.Navigation.PushAsync(new MainPage());
+        }
+    }
+}
